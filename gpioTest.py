d1c1003c42ce68e1fd46066b523e673a2444979f
Leon Kolanowski <lkolanowski3@gmail.com>
2020-06-13 17:26:42 +0200

    Error fix

--- gpioTest.py
+++ gpioTest.py
@@ -1,30 +1,36 @@
 from gpiozero import LED
 from time import sleep
-from multiprocessing import Process
+from threading import Thread, Lock
+
+class GlobalVars():
+    pass
 
 def blinkLed():
     led = LED(2)
     while True:
+        if(g.kill):
+            return
         led.on()
         sleep(1)
         led.off()
         sleep(1)
 
-p1 = Process(target=blinkLed)
-blinkLed()
+g = GlobalVars()
+g.kill = False
+t1 = Thread(target=blinkLed, daemon=True)
 
-#while True:
- #   print("What do you want to do?")
-  #  s = input()
-   # if(s == "start"):
-    #    p1.start
-     #   p1.run
-    #elif(s == "stop"):
-     #   p1.kill    
-    #elif(s == "exit"):
-     #   exit(0)
-    #else:
-     #   print("That is not an option!")
+while True:
+    print("What do you want to do?")
+    s = input()
+    if(s == "start"):
+        g.kill = False
+        t1.start
+    elif(s == "stop"):
+        g.kill = True  
+    elif(s == "exit"):
+        exit(0)
+    else:
+        print("That is not an option!")
 
 
 
