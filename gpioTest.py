8434980045cc8c473f116b27bfbed72be2ec91b2
Leon Kolanowski <lkolanowski3@gmail.com>
2020-06-13 16:42:45 +0200

    Multiprocessing

--- gpioTest.py
+++ gpioTest.py
@@ -1,10 +1,34 @@
 from gpiozero import LED
 from time import sleep
+from multiprocessing import Process
 
-led = LED(2)
+def blinkLed():
+    led = LED(2)
+    while True:
+        led.on()
+        sleep(1)
+        led.off()
+        sleep(1)
+
+p1 = Process(target=blinkLed, daemon=True)
 
 while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+    print("What do you want to do?")
+    s = input()
+    if(s == "start"):
+        if(p1.is_alive):
+            print("Blink is already running!")
+        else:
+            p1.start()
+    elif(s == "stop"):
+        if(p1.is_alive):
+            p1.kill()
+        else:
+            print("Blink isn't running!")
+    elif(s == "exit"):
+        exit(0)
+    else:
+        print("That is not an option!")
+
+
+
